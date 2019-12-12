49dedadf95a6c5a09e1a2a1c1fa7f87f8a8faee5
AmirGiz <51257858+AmirGiz@users.noreply.github.com>
2019-12-12 08:55:09 +0300

    new bd

--- CarPartsStore/Migrations/20191212055224_Initial.Designer.cs
+++ CarPartsStore/Migrations/20191212055224_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CarPartsStore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20191212001026_Initial")]
+    [Migration("20191212055224_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
